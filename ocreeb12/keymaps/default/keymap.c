3650e313aa7f52c26217e867e5128c88d094acf2
MadJalapeno <github@jalapeno.co.uk>
2023-03-14 14:27:32 -0400

    encoders semi working

--- ocreeb12/keymaps/default/keymap.c
+++ ocreeb12/keymaps/default/keymap.c
@@ -17,23 +17,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT(
         KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_P0,-
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS
+        KC_P4,   KC_P5,   KC_P6,   KC_P0,
+        RGB_TOG,   KC_P2,   KC_P3,   KC_PMNS
     )
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) { /* First encoder */
         if (clockwise) {
-            tap_code_delay(KC_VOLU, 2);
+            tap_code_delay(KC_VOLU, 20);
         } else {
-            tap_code_delay(KC_VOLD, 2);
+            tap_code_delay(KC_VOLD, 20);
         }
     } else if (index == 1) { /* Second encoder */
         if (clockwise) {
-            tap_code_delay(RGB_MODE_FORWARD);
+            tap_code(RGB_HUI);
         } else {
-            tap_code_delay(RGB_MODE_REVERSE);
+            tap_code(RGB_HUD);
         }
     }
     return false;
