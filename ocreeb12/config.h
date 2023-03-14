2ab4df5a25a1bcae798efb8cfefd6864c62272ec
MadJalapeno <github@jalapeno.co.uk>
2023-03-13 21:15:06 -0400

    added encoders

--- ocreeb12/config.h
+++ ocreeb12/config.h
@@ -19,5 +19,24 @@
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
 
+// Adafruit KB2040 Pinout
+// https://cdn-learn.adafruit.com/assets/assets/000/106/984/original/adafruit_products_Adafruit_KB2040_Pinout.png?1638564074
+
+// RGB LED setup
 #define RGB_DI_PIN GP10
 #define RGBLED_NUM 4
+
+//Encoder setup
+// kmk setup
+// encoders.pins = ((board.A2, board.A1, board.A0, False), (board.SCK, board.MISO, board.MOSI, False),)
+// A2 = GP28
+// A1 = GP27
+// A0 = GP26
+//
+// SCK = GP18
+// MISO= GP20
+// MOSI= GP19
+//
+#define ENCODERS_PAD_A { GP27, GP28 }
+#define ENCODERS_PAD_B { GP28, GP19 }
+#define ENCODER_RESOLUTION 4
--- ocreeb12/keymaps/default/keymap.c
+++ ocreeb12/keymaps/default/keymap.c
@@ -17,8 +17,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT(
         KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_P0,
+        KC_P4,   KC_P5,   KC_P6,   KC_P0,-
         KC_P1,   KC_P2,   KC_P3,   KC_PMNS
     )
 };
 
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 2);
+        } else {
+            tap_code_delay(KC_VOLD, 2);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code_delay(RGB_MODE_FORWARD);
+        } else {
+            tap_code_delay(RGB_MODE_REVERSE);
+        }
+    }
+    return false;
+}
